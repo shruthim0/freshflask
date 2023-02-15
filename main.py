5547aa0fac44c5d0bfc8fd10213e66477f39169b
jiya-sav <starjiyu@gmail.com>
2023-02-14 22:28:20 -0800

    api recipes

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ from __init__ import app  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.scores import initScores
+from model.recipes import initRecipes
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.score import score_api
+from api.recipe import recipe_api
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -24,6 +26,7 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(score_api)
+app.register_blueprint(recipe_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -43,6 +46,7 @@ def activate_job():
     initJokes()
     initUsers()
     initScores()
+    initRecipes()
 
 # this runs the application on the development server
 if __name__ == "__main__":
